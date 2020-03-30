f1d41972cc50d789dbd58d83005da77775ce906e
mikaelbeat <ryynanenphm@gmail.com>
2020-03-30 22:55:17 +0300

    Update

---
+++ Read_validate_write.ps1
@@ -0,0 +1,11 @@
+﻿
+[validateLength(3, 10)][string]$name = Read-Host "What is your name: "
+[validaterange(18, 90)][int]$age = Read-Host "What is your age: "
+[validateset("y", "Y")]$response = Read-Host "Is PowerShell awesome? (y, n) "
+$password = Read-Host "Enter you password: " -AsSecureString
+
+Write-Output "Your name is $name and your age is $age!"
+
+
+Write-Host "Yellow text!" -ForegroundColor Yellow
+Write-Host "Yellow text, red background!" -ForegroundColor Yellow -BackgroundColor Red
--- Variables.ps1
+++ Variables.ps1
@@ -5,9 +5,9 @@ $first_variable
 # Global variable
 $global:another_variable = "This can be accessed outside of the script!"
 
-Set-Variable -Name "first_variable" -Value "This is my first variable overwritten!"
+#Set-Variable -Name "first_variable" -Value "This is my first variable overwritten!"
 
-New-Variable -Name "new_variable" -Value "This is my new variable!"
+#New-Variable -Name "new_variable" -Value "This is my new variable!"
 
 
 #Remove-Variable -Name "variable"
@@ -16,6 +16,33 @@ New-Variable -Name "new_variable" -Value "This is my new variable!"
 
 
 # Setting variable to be constant
-Set-Variable const_variable -Option Constant -Value 100
+#Set-Variable const_variable -Option Constant -Value 100
 
-Set-Variable readonly_variable -Option ReadOnly -Value 100
+#Set-Variable readonly_variable -Option ReadOnly -Value 100
+
+$Int_variable = 10
+$Int_variable.GetType()
+
+$Str_variable = "Hello"
+$Str_variable.GetType()
+
+$Str_Upper = $Str_variable.ToUpper()
+Write-Output $Str_Upper
+
+$Str_replaced = $Str_variable.Replace("e", "a")
+Write-Output $Str_replaced
+
+
+$Data = "$Str_replaced, nice to meet you!"
+Write-Output $Data
+
+
+$Str_variable -is [string]
+$Str_variable -is [int]
+
+$Int_variable -is [int]
+$Int_variable -is [string]
+
+
+# Type cast
+$Some_value = [int]100
