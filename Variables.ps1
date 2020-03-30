8b3a5a1cd510a718b934923892e076ac7d09bc5e
mikaelbeat <ryynanenphm@gmail.com>
2020-03-30 16:21:59 +0300

    New changes

--- Variables.ps1
+++ Variables.ps1
@@ -1,6 +1,21 @@
 ﻿
-$My_variable = "This is my variable"
-$My_variable
+$first_variable = "This is my variable"
+$first_variable
 
 # Global variable
-$global:another_variable = "This can be accessed in all script"
+$global:another_variable = "This can be accessed outside of the script!"
+
+Set-Variable -Name "first_variable" -Value "This is my first variable overwritten!"
+
+New-Variable -Name "new_variable" -Value "This is my new variable!"
+
+
+#Remove-Variable -Name "variable"
+
+#Clear-Variable -Name "variable" -> Clears variable value but leaves variable
+
+
+# Setting variable to be constant
+Set-Variable const_variable -Option Constant -Value 100
+
+Set-Variable readonly_variable -Option ReadOnly -Value 100
