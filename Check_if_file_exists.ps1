95a0492587ce9463fa551197a644555babf47b89
mikaelbeat <ryynanenphm@gmail.com>
2020-04-02 22:06:29 +0300

    Great things added!

---
+++ Check_if_file_exists.ps1
@@ -0,0 +1,26 @@
+﻿
+clear
+
+Set-Location D:\Git\github\Advanced_scripting_with_PowerShell\
+
+$exist = false
+
+while ($exist == false) {
+
+    if (Test-Path .\Kissa.txt) {
+        Write-Output "File exist!"
+        $exist = true
+    } else {
+        Write-Output "File doesn't exist!"
+    }
+
+}
+
+
+
+while (!(Test-Path "Kissa.txt")) { 
+    Start-Sleep 1 
+    Write-Output "Waiting for the file to appear"
+}
+
+Write-Output "File found!"
