10b1515e8c24b048b81f12c61bc535a7288e3f9e
mikaelbeat <ryynanenphm@gmail.com>
2020-04-02 17:10:34 +0300

    New stuff

---
+++ XML_JSON_file_handling.ps1
@@ -0,0 +1,33 @@
+﻿
+clear
+
+Set-Location D:\Git\github\Advanced_scripting_with_PowerShell\
+
+# Read XML file
+[xml]$xml_data = Get-Content .\Sample.xml
+
+$xml_data.GetType()
+
+# Get main nodes
+$xml_data.breakfast_menu
+
+# Get main nodes first element
+$xml_data.breakfast_menu.food[0]
+
+# Get main nodes first elements price
+$xml_data.breakfast_menu.food[0].price
+
+
+Write-Output "" 
+Write-Output " ------------- JSON Training ------------- "
+Write-Output ""
+
+
+# Read JSON file
+$json_data = Get-Content .\Sample.json | Out-String | ConvertFrom-Json
+
+$json_data.GetType()
+
+# Get value from JSON
+$json_data.id
+$json_data.image.url
