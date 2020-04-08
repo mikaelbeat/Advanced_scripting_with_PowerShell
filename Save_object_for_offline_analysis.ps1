d030d7049b2008aecd0ecb1ef00240a31f472ffd
mikaelbeat <ryynanenphm@gmail.com>
2020-04-08 15:17:02 +0300

    Update

---
+++ Save_object_for_offline_analysis.ps1
@@ -0,0 +1,8 @@
+﻿
+clear
+
+Set-Location D:\Git\github\Advanced_scripting_with_PowerShell
+
+Get-Process | Select-Object -Last 4 | Export-Clixml "Process_objects.xml"
+
+$data = Import-Clixml Process_objects.xml | Select-Object -Last 2
---
+++ Sending_Email.ps1
@@ -0,0 +1,34 @@
+﻿	# Details of the Login Username and SMTP server. These can very from one 
+	# Mail Service Provder to another
+	$SMTPServer = "smtp.gmail.com"
+	$SMTPPort = "587"
+	$Username = "testingpurpose201801@gmail.com"
+	$Password = "TestingPurpose_forStudents"
+
+	
+	#Add email address of person in to & cc  whom you are sending email (Multiple addresses should be comma seperated)
+	$to = "testingpurpose201801@gmail.com"
+	$cc = "testingpurpose201801@gmail.com"
+
+
+	$subject = "Email Subject"
+	$body = "Dear Sir/Madam, 
+			This is a system generated email. Please do not reply.
+
+			Information: This is the important information
+			Regards,
+			Team XYZ
+			"
+
+	$message = New-Object System.Net.Mail.MailMessage
+	$message.subject = $subject
+	$message.body = $body
+	$message.to.add($to)
+	$message.cc.add($cc)
+	$message.from = "testingpurpose201801@gmail.com"
+
+$smtp = New-Object System.Net.Mail.SmtpClient($SMTPServer, $SMTPPort);
+$smtp.EnableSSL = $true
+$smtp.Credentials = New-Object System.Net.NetworkCredential($Username, $Password);
+
+$smtp.send($message)
